0ee28a6c44bf3b936ed63e910d128dc20482e481
Chandni P <chandnirpatel@gmail.com>
2015-04-04 09:57:08 -0700

    Add test class

---
+++ MoneyTest.java
@@ -0,0 +1,100 @@
+/** Money Tests
+ * Created by Chandni on 4/3/15.
+ */
+
+import junit.framework.TestCase;
+
+public class MoneyTest extends TestCase {
+
+    public void testTestMultiplicationDollar(){
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    public void testEquality(){
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    public void testCurrency(){
+        assertEquals("USD", Money.dollar(1).currency);
+        assertEquals("CHF", Money.franc(1).currency);
+    }
+
+    public void testSimpleAddition(){
+        Money five= Money.dollar(5);
+        Expression sum= five.plus(five);
+        Bank bank = new Bank();
+        Money reduced= bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
+
+    public void testPlusReturnsSum(){
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    public void testReduceSum(){
+        Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    public void testReduceMoney(){
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    public void testReduceMoneyDifferentCurrencies(){
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    public void testIdentityRate(){
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
+    public void testMixedAddtion() throws Exception{
+        Expression fiveBucks= Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);;
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
+    public void testSumPlusMoney() throws Exception{
+        Expression fiveBucks= Money.dollar(5);
+        Expression tenFrancs= Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum= new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result= bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
+
+    public void testSumTimes(){
+        Expression fiveBucks= Money.dollar(5);
+        Expression tenFrancs= Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum= new Sum(fiveBucks, tenFrancs).times(2);
+        Money result= bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(20), result);
+    }
+
+//    public void testPlusSameCurrencyReturnsMoney() throws Exception{
+//        Expression sum= Money.dollar(1).plus(Money.dollar(1));
+//        assertTrue(sum instanceof Money);
+//    }
+
+}
